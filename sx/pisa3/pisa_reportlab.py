2400d6ac9bf449beb1a7a2422018d024d1809d16
Denis Malinovskiy <denis@drchrono.com>
2017-12-01 14:01:34 -0800

    Check for empty transparency palette

--- sx/pisa3/pisa_reportlab.py
+++ sx/pisa3/pisa_reportlab.py
@@ -352,6 +352,8 @@ class PmlImageReader(object):
             if self._image.info.has_key("transparency"):
                 transparency = self._image.info["transparency"] * 3
                 palette = self._image.palette
+                if not palette:
+                    return None
                 try:
                     palette = palette.palette
                 except SoftTimeLimitExceeded:
